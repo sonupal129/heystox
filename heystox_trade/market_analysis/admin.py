c3b3edc063647d4092310ce9948b8b64644f6ae6
sonu <sonu.pal@gamehunter.in>
2019-09-23 23:23:52 +0530

    Added New Function and data model to projects

--- heystox_trade/market_analysis/admin.py
+++ heystox_trade/market_analysis/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from market_analysis.models import (Candle, UserProfile)
+from market_analysis.models import (Candle, UserProfile, MasterContract, Symbol)
 # Register your models here.
 
-admin.site.register(Candle)
+class SymbolAdmin(admin.ModelAdmin):
+      list_display = ["symbol", "exchange"]
+      list_filter = ["exchange"]
+      search_fields = ["symbol", "name"]
+
+class CandleAdmin(admin.ModelAdmin):
+      list_display = ["symbol", "exchange", "candle_type"]
+      list_filter = ["candle_type"]
+
+      def exchange(self, obj):
+            return obj.symbol.exchange
+
+admin.site.register(Candle, CandleAdmin)
 admin.site.register(UserProfile)
+admin.site.register(Symbol, SymbolAdmin)
+admin.site.register(MasterContract)
--- heystox_trade/market_analysis/models.py
+++ heystox_trade/market_analysis/models.py
@@ -23,6 +23,39 @@ from django.contrib.postgres.fields import JSONField
 #       asks = JSONField()
 #       ltt = models.IntegerField("Last Traded Price", blank=True, null=True)
 
+class CandleQuerySet(models.QuerySet):
+      def get_by_candle_type(self, type_of_candle):
+            return self.filter(candle_type=type_of_candle)
+
+
+class CandleManager(models.Manager):
+      def get_queryset(self):
+            return CandleQuerySet(self.model, using=self._db).order_by("date")
+      
+      def get_candles(self, candle_type="M5", date=None):
+            if date:
+                  return self.get_queryset.get_by_candle_type(candle_type, date__gte=date)
+            return self.get_queryset().get_by_candle_type(candle_type)
+
+class MasterContract(models.Model):
+      name = models.CharField(max_length=50)
+
+      def __str__(self):
+            return self.name
+
+class Symbol(models.Model):
+      exchange = models.ForeignKey(MasterContract, related_name="symbols", on_delete=models.DO_NOTHING)
+      token = models.IntegerField(blank=True, null=True)
+      symbol = models.CharField(max_length=100)
+      name = models.CharField(max_length=100)
+      closing_price = models.FloatField(blank=True, null=True)
+      tick_size = models.FloatField(blank=True, null=True)
+      instrument_type = models.CharField(max_length=20, blank=True, null=True)
+      isin = models.CharField(max_length=50)
+
+      def __str__(self):
+            return self.symbol
+
 class Candle(models.Model):
       candle_type_choice = {
             ("M5", "5 Minute"),
@@ -31,21 +64,32 @@ class Candle(models.Model):
             ("M60", "60 Minute"),
             ("1D", "1 Day"),
       }
-
+      symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE, related_name="candles", null=True)
       candle_type = models.CharField(choices=candle_type_choice, max_length=50, blank=False, null=True, default=None)
-      open_price = models.IntegerField("Candle Open Price", blank=True, null=True)
-      high_price = models.IntegerField("Candle High Price", blank=True, null=True)
-      low_price = models.IntegerField("Candle Low Price", blank=True, null=True)
-      close_price = models.IntegerField("Candle Close Price", blank=True, null=True)
-      ltt = models.IntegerField("Candle Last Traded Time", blank=True, null=True)
+      open_price = models.FloatField("Candle Open Price", blank=True, null=True)
+      high_price = models.FloatField("Candle High Price", blank=True, null=True)
+      low_price = models.FloatField("Candle Low Price", blank=True, null=True)
+      close_price = models.FloatField("Candle Close Price", blank=True, null=True)
+      volume = models.IntegerField("Volume", blank=True, null=True)
       date = models.DateTimeField()
+
+      objects = CandleManager()
+
+      def __str__(self):
+            return self.symbol.symbol
+
 
 class UserProfile(models.Model):
       email = models.CharField(max_length=100)
-      mobile = models.IntegerField(max_length=10, blank=True, null=True)
+      mobile = models.IntegerField(blank=True, null=True)
       api_key = models.CharField(max_length=100, null=True, blank=True)
       secret_key = models.CharField(max_length=100, null=True, blank=True)
       redirect_url = models.CharField(max_length=100, null=True, blank=True)
       response_code = models.CharField(max_length=100, null=True, blank=True)
+      access_token = models.CharField(max_length=100, null=True, blank=True)
+      client_id = models.IntegerField(blank=True, null=True)
+      bank_name = models.CharField(max_length=100, blank=True, null=True)
+      bank_account = models.CharField(max_length=100, blank=True, null=True)
 
-
+      def __str__(self):
+            return self.email or self.client_id
