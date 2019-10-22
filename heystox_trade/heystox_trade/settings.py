4fd655c16f5c0005939e345995234789e406c83a
Makki <makkii.dev@gmail.com>
2019-10-23 00:25:23 +0530

    resolved merge conflicts

--- heystox_trade/heystox_trade/settings.py
+++ heystox_trade/heystox_trade/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'heystox_trade.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'testdb',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '123456',                  # Not used with sqlite3.
+        'NAME': 'test_db',                      # Or path to database file if using sqlite3.
+        'USER': 'heystox',                      # Not used with sqlite3.
+        'PASSWORD': 'fedora',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
