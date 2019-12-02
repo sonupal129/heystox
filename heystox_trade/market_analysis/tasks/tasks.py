1050e64deb088760676e92c1a89762f8592388e3
sonu2 <sonu2@Sonu-ECHO.local>
2019-12-02 23:27:06 +0530

    Tested, Optimized, Changed and Verified fuction to get them run properly

---
+++ heystox_trade/market_analysis/tasks/tasks.py
@@ -0,0 +1,11 @@
+from market_analysis.slack import send_slack_message
+from celery.task import periodic_task
+from celery.schedules import crontab
+from celery.decorators import task
+
+# from heystox_intraday.intraday_fetchdata import update_symbol_data, update_all_symbol_candles
+# Code Starts Below
+
+@task(name="send_slack_message")
+def slack_message_sender(channel='#heystox', text='Message', attachments=None):
+    send_slack_message(channel, text, attachments)
