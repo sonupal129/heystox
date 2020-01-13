d89ab896e8f9d318b8002e47bcdc0fad20fc8cd8
sonupal129 <sonupal129@gmail.com>
2020-01-13 23:44:51 +0530

    Made changed in macd and stochastic function

--- heystox_trade/heystox_trade/urls.py
+++ heystox_trade/heystox_trade/urls.py
@@ -23,6 +23,6 @@ admin.site.index_title = "Welcome to Heystox"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("market_analysis.urls", namespace="market_analysis_urls")),
-    path('api/', include("market_analysis.api_urls", namespace="market_analysis_api_urls"))
+    path('api/v1/', include("market_analysis.api_urls", namespace="market_analysis_api_urls"))
 ]
 
--- heystox_trade/market_analysis/api_views.py
+++ heystox_trade/market_analysis/api_views.py
@@ -1,7 +1,7 @@
-from market_analysis.serializers import UserSerializer
+from market_analysis.serializers import UserSerializer, UserProfileSerializer
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
-
+from .models import UserProfile
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
@@ -14,49 +14,14 @@ class UsersListView(APIView):
     permission_classes = [IsAuthenticated]
     
     def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        users = UserProfile.objects.all()
+        serializer = UserProfileSerializer(users, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@csrf_exempt
-def users_view(request):
-    if request.method == "GET":
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == "POST":
-        post_data = JSONParser().parse(request)
-        serializer = UserSerializer(data=post_data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-@csrf_exempt  
-def user_detail_view(request, pk):
-    try:
-        instance = User.objects.get(id=pk)
-    except User.DoesNotExist:
-        return JsonResponse({"error": "User Id is not available"}, status=404)
-    if request.method == "GET":
-        serializer = UserSerializer(instance)
-        return JsonResponse(serializer.data)
-    elif request.method == "PUT":
-        post_data = JSONParser().parse(request)
-        serializer = UserSerializer(instance, data=post_data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=200)
-        return JsonResponse(serializer.errors, status=400)
-    elif request.method == "DELETE":
-        instance.delete()
-        return JsonResponse({"succesfull":"User Deleted Successfully"}, status=204)
+    # def post(self, request):
+    #     serializer = UserProfileSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
