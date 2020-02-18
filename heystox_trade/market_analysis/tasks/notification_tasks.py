6c1d3c81cd2c4125c365e62d12b39fb0eb10739d
sonupal129 <sonupal129@gmail.com>
2020-02-18 09:56:02 +0530

    FIX SLACK

--- heystox_trade/market_analysis/tasks/notification_tasks.py
+++ heystox_trade/market_analysis/tasks/notification_tasks.py
@@ -6,7 +6,7 @@ from heystox_trade import settings
 @shared_task(queue="low_priority")
 def slack_message_sender(channel='#heystox', text='Message', attachments=None):
     """Send Slack notification to user"""
-    if settings.DEBUG:
+    if not settings.DEBUG:
         client = slack.WebClient(token=settings.SLACK_TOKEN)
         response = client.chat_postMessage(
             channel=channel,
